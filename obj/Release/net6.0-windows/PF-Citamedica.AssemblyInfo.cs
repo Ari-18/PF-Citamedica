3ff8ca5d94effb72da764aab143bdca384a2bdfa
Ari-18 <guzmanariela7@gmail.com>
2022-07-09 13:08:48 -0400

    Actualizacion

--- obj/Release/net6.0-windows/PF-Citamedica.AssemblyInfo.cs
+++ obj/Release/net6.0-windows/PF-Citamedica.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PF-Citamedica")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PF-Citamedica")]
