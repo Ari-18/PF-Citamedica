880761d27c2c58d4ecb3eec39c402bfb381cc747
wagnermorillo <1107665@est.intec.edu.do>
2022-07-10 04:26:47 -0400

    menu

---
+++ WAG-menuPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PF_Citamedica
+{
+    public partial class WG_menuPrincipal : Form
+    {
+        public WG_menuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
